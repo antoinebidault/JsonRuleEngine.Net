a95cde3c44b8d7d2430ff1dc9832b80b83951019
Bidault <an_bidoche@hotmail.com>
2020-11-11 00:59:04 +0100

    first commit

---
+++ JsonRuleEngine.Net.Tests/test.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace JsonRuleEngine.Net.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ JsonRuleEngine/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JsonRule.Net
+{
+    public class Class1
+    {
+    }
+}
